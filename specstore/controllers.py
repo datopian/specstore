113a8d3cad0ea509ba0ac2fe54597829749acc1f
zelima <irakli.mchedlishvili@datopian.com>
2017-10-25 12:35:31 +0400

    [flowmanager][l]: refactor to handle FlowManager registry
    * replace SourceSPecRegistry for FlowRegistry
    * refactor existing controlers to handle new registry
    * refacto tests according to changes
    * new controler for handling update
    * tests
    refs - #9
    ~

--- specstore/controllers.py
+++ specstore/controllers.py
@@ -1,11 +1,13 @@
 import datetime
 import jwt
 import requests
-from datapackage_pipelines_sourcespec_registry.registry import SourceSpecRegistry
+
+import planner
 from werkzeug.exceptions import NotFound
 
 from .config import dpp_module, dpp_server
 from .config import dataset_getter, owner_getter, update_time_setter
+from .models import FlowRegistry
 
 
 def _verify(auth_token, owner, public_key):
@@ -31,9 +33,9 @@ def _verify(auth_token, owner, public_key):
         return False
 
 
-def upload(token, contents, registry: SourceSpecRegistry, public_key):
+def upload(token, contents, registry: FlowRegistry, public_key):
     errors = []
-    uid = None
+    dataset_id = None
     if contents is not None:
         owner = owner_getter(contents)
         if owner is not None:
@@ -42,8 +44,25 @@ def upload(token, contents, registry: SourceSpecRegistry, public_key):
                     dataset_name = dataset_getter(contents)
                     now = datetime.datetime.now()
                     update_time_setter(contents, now)
-                    uid = registry.put_source_spec(dataset_name, owner, dpp_module, contents,
-                                                   ignore_missing=True, now=now)
+                    dataset_id = registry.format_identifier(owner, dataset_name)
+                    registry.create_or_update_dataset(
+                        dataset_id, owner, contents, now)
+                    revision = registry.create_revision(
+                        dataset_id, now, 'flow-pending', errors)
+                    revision = revision['revision']
+                    pipelines = planner.plan(revision, contents)
+                    for pipeline in pipelines:
+                        doc = dict(
+                            pipeline_id=registry.format_identifier(
+                                owner, dataset_name, revision, pipeline[0]),
+                            flow_id=registry.format_identifier(
+                                owner, dataset_name, revision),
+                            pipeline_details=pipeline,
+                            status='pending',
+                            errors=errors,
+                            updated_at=now
+                        )
+                        registry.save_pipeline(doc)
                 except ValueError as e:
                     errors.append('Validation failed for contents')
             else:
@@ -55,13 +74,42 @@ def upload(token, contents, registry: SourceSpecRegistry, public_key):
 
     return {
         'success': len(errors) == 0,
-        'id': uid,
+        'id': dataset_id,
+        'errors': errors
+    }
+
+def update(content, registry: FlowRegistry):
+    errors = content['errors']
+    now = datetime.datetime.now()
+    pipeline_id = content['pipeline']
+    event = content['event']
+    pipeline_status = 'pending'
+    if content['success'] and event == 'finished':
+        pipeline_status = 'success'
+    elif (not content['success']) and  event == 'finished':
+        pipeline_status = 'failed'
+    doc = dict(
+        status=pipeline_status,
+        errors=errors,
+        updated_at=now
+    )
+    registry.update_pipeline(pipeline_id, doc)
+    flow_id = registry.get_flow_id(pipeline_id)
+    flow_status = registry.check_flow_status(flow_id)
+    doc['status'] = flow_status
+    registry.update_revision(flow_id, doc)
+    if flow_status == 'success':
+        registry.delete_pipelines(flow_id)
+
+    return {
+        'status': flow_status,
+        'id': flow_id,
         'errors': errors
     }
 
 
-def get_fixed_pipeline_state(owner, dataset, registry: SourceSpecRegistry):
-    spec = registry.get_source_spec(SourceSpecRegistry.format_uid(owner, dataset))
+def get_fixed_pipeline_state(owner, dataset, registry: FlowRegistry):
+    spec = registry.get_dataset(FlowRegistry.format_identifier(owner, dataset))
     if spec is None:
         raise NotFound()
     resp = requests.get(dpp_server + 'api/raw/{}/{}'.format(owner, dataset))
@@ -74,14 +122,14 @@ def get_fixed_pipeline_state(owner, dataset, registry: SourceSpecRegistry):
         update_time = resp.get('pipeline', {}).get('update_time')
         if update_time is None:
             update_time = ''
-        if spec.updated_at and spec.updated_at.isoformat() > update_time:
+        if spec['updated_at'] and spec['updated_at'].isoformat() > update_time:
             resp['state'] = 'REGISTERED'
-    resp['spec_contents'] = spec.contents
-    resp['spec_modified'] = spec.updated_at.isoformat()
+    resp['spec_contents'] = spec['spec']
+    resp['spec_modified'] = spec['updated_at'].isoformat()
     return resp
 
 
-def status(owner, dataset, registry: SourceSpecRegistry):
+def status(owner, dataset, registry: FlowRegistry):
     resp = get_fixed_pipeline_state(owner, dataset, registry)
     logs = resp.get('reason', '')
     if logs is None:
@@ -95,7 +143,6 @@ def status(owner, dataset, registry: SourceSpecRegistry):
     }
 
 
-def info(owner, dataset, registry: SourceSpecRegistry):
+def info(owner, dataset, registry: FlowRegistry):
     resp = get_fixed_pipeline_state(owner, dataset, registry)
     return resp
-
