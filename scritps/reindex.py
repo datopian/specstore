d605a9d4d796a9059ad91a3fc17f6565b279a852
Irakli Mchedlishvili <you@example.com>
2021-12-24 11:36:14 +0400

    [reindex][s]: make sure we include datahub info

--- scritps/reindex.py
+++ scritps/reindex.py
@@ -6,19 +6,20 @@ from flowmanager.models import FlowRegistry
 
 DB_STRING = os.environ['FILEMANAGER_DATABASE_URL']
 
-
-
 fr = FlowRegistry(DB_STRING)
-
 
 for dataset in fr.list_datasets():
     dataset = FlowRegistry.object_as_dict(dataset)
+    datahub = dataset['spec'].get('meta')
+    datahub['created'] = dataset.get('created_at')
+    datahub['modified'] = dataset.get('updated_at')
     descriptor = dataset['spec']['inputs'][0]['parameters'].get('descriptor', {})
     send_dataset(
-                        dataset.get('identifier'),
-                        descriptor.get('name'),
-                        descriptor.get('title'),
-                        descriptor.get('description'),
-                        descriptor.get('datahub'),
-                        descriptor,
-                        dataset.get('certified') or False)
+        dataset.get('identifier'),
+        descriptor.get('name'),
+        descriptor.get('title'),
+        descriptor.get('description'),
+        datahub,
+        descriptor,
+        dataset.get('certified') or False
+    )
